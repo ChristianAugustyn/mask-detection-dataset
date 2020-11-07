79a545a6b5e7d57618732ccebd7c3a41409a568d
Christian <chrisaug@rogers.com>
2020-11-07 13:55:32 -0500

    prepared data using script to perform numpy arrays

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,8 @@
-import os, cv2
-import
+import os, cv2, numpy
+from keras.utils as ku 
 
 path = 'data'
-
+size = 100
 labels_obj = {
     'with_mask': 0,
     'without_mask': 1
@@ -13,21 +13,25 @@ categories = ['with_mask', 'without_mask']
 data = list()
 target = list()
 
-for ctg in categories:
-    folder = os.path.join(path, ctg)
-    imgs = os.listdir(folder)
+for ctg in categories: #loops over the two folders, one of people with mask and one without masks
+    folder = os.path.join(path, ctg) #gets the folder path for
+    imgs = os.listdir(folder) #gets th elist of images in the folders path
 
     for i in imgs:
-        i_path = os.path.join(folder, i)
-        img = cv2.imread(i_path)
+        i_path = os.path.join(folder, i) #gets image path
+        img = cv2.imread(i_path) #uses cv2 to read the image
 
         try:
-            resize = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),(100,100))
+            resize = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),(size,size)) #turns the image to grey scale and resizes it to 100x100
             data.append(resize)
             target.append(labels_obj[ctg])
         except Exception as e:
             print(e)
 
+data = numpy.array(data) / 255 #makes the images in the range from 0-1
+data = numpy.reshape(data, (data.shape[0], size, size, 1)) #reshapes the numpy array as a 4d array to be used in the CNN
+target = numpy.array(target)
+target = ku.np_utils.to_categorical(target)
 
-cv2.imshow("test", data[0])
-cv2.waitKey(0)  
+numpy.save('data', data)
+numpy.save('target', target)
